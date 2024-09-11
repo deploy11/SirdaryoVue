d40d742d52a3c9a0bd3023f037274a0fd83e73ea
deploy11 <129506514+deploy11@users.noreply.github.com>
2024-09-11 18:02:23 +0500

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -22,7 +22,7 @@
       </div>
     </div>
     <div class="footer-bottom">
-      <p>&copy; 2024 Your Company. All rights reserved.</p>
+      <p>&copy; 2024 <a href="https://nurazizcommchj.pythonanywhere.com">Nurazizcom<a>. Barcha huquqlar himoyalangan</p>
     </div>
   </footer>
 </template>
