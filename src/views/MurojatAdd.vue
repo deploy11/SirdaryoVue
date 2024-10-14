0f1c377a759e5a945da8dc569f1758c368f5e9cd
deploy11 <129506514+deploy11@users.noreply.github.com>
2024-10-14 13:23:35 +0500

    Update MurojatAdd.vue

--- src/views/MurojatAdd.vue
+++ src/views/MurojatAdd.vue
@@ -164,7 +164,7 @@ export default {
       const arizaTuriKril = this.arizaTurlari.find(turi => turi.code === this.selectedArizaTuri)?.labelKril;
 
       try {
-        const response = await fetch('https://sirdarya777.pythonanywhere.com/api/', {
+        const response = await fetch('https://sirdaryo-new-production.up.railway.app/api/', {
           method: 'POST',
           body: JSON.stringify({
             fish: this.fish,
